93b2dab78822e4d81d693b196f674a581f554c79
Yoonssu <orvosdoktor21@gmail.com>
2024-01-31 00:00:11 +0900

    시간 저장 모델

---
+++ models/user_activities.js
@@ -0,0 +1,36 @@
+const { DataTypes, Model, Sequelize } = require('sequelize');
+
+module.exports = class UserActivity extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init(
+            {
+                user_id: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                category: {
+                    type: DataTypes.STRING(255),
+                    allowNull: false
+                },
+                duration_minutes: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                activity_date: {
+                    type: DataTypes.DATE,
+                    allowNull: false
+                }
+            },
+            {
+                sequelize,
+                modelName: 'UserActivity',
+                tableName: 'user_activities',
+                timestamps: false // timestamps를 사용하지 않을 경우
+            }
+        );
+    }
+
+    static associate(models) {
+        // UserActivity 모델과 다른 모델 간의 관계를 정의할 수 있음
+    }
+};
