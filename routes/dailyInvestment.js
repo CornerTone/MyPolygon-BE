8f8475e681fb955b675cc1fac870c2d4f6527c47
Yoonssu <orvosdoktor21@gmail.com>
2024-02-02 00:38:44 +0900

    fix

--- routes/dailyInvestment.js
+++ routes/dailyInvestment.js
@@ -26,7 +26,7 @@ router.post('/save_daily', auth, async (req, res) => {
 });
 
 // 시간 수정될 시 저장
-router.post('/rewrite_daily', auth, async (req, res) => {
+router.put('/rewrite_daily', auth, async (req, res) => {
     try {
         const { category, timeInvested, activityDate } = req.body;
         const user_id = req.user.id; // 요청에서 사용자 ID를 가져옵니다. 이는 로그인된 사용자의 ID일 것입니다.
@@ -61,12 +61,12 @@ router.post('/rewrite_daily', auth, async (req, res) => {
     }
 });
 
-// GET 요청을 통해 특정 사용자의 하루 투자 정보를 조회하는 라우터
-router.get('/daily/:userId', async (req, res) => {
+// GET 요청을 통해 현재 사용자의 하루 투자 정보를 조회하는 라우터
+router.get('/daily', auth, async (req, res) => {
     try {
-        const userId = req.params.userId;
+        const userId = req.user.id; // 인증된 사용자의 ID를 가져옵니다.
 
-        // DailyInvestment 모델을 사용하여 특정 사용자의 하루 투자 정보를 조회
+        // DailyInvestment 모델을 사용하여 현재 사용자의 하루 투자 정보를 조회
         const userDailyInvestments = await DailyInvestment.findAll({
             where: {
                 user_id: userId,
@@ -76,11 +76,12 @@ router.get('/daily/:userId', async (req, res) => {
             }
         });
 
-        res.status(200).json({ success: true, message: '사용자의 하루 투자 정보를 조회했습니다.', data: userDailyInvestments });
+        res.status(200).json({ success: true, message: '현재 사용자의 하루 투자 정보를 조회했습니다.', data: userDailyInvestments });
     } catch (error) {
         console.error('Error fetching user daily investments:', error);
-        res.status(500).json({ success: false, message: '사용자의 하루 투자 정보를 조회하는 데 실패했습니다.' });
+        res.status(500).json({ success: false, message: '현재 사용자의 하루 투자 정보를 조회하는 데 실패했습니다.' });
     }
 });
+
 
 module.exports = router;
