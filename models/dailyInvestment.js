b6250226b4b4c2f6cfabfb553dec80d24128a6af
Yoonssu <orvosdoktor21@gmail.com>
2024-02-05 13:39:52 +0900

    분 모델 변결

--- models/dailyInvestment.js
+++ models/dailyInvestment.js
@@ -1,16 +1,18 @@
-// models/dailyInvestment.js
 const { DataTypes, Model, Sequelize } = require('sequelize');
 
 module.exports = class DailyInvestment extends Sequelize.Model {
     static init(sequelize) {
         return super.init(
             {
-
                 category: {
                     type: DataTypes.STRING,
                     allowNull: false
                 },
-                timeInvested: {
+                hours: {
+                    type: DataTypes.INTEGER,
+                    allowNull: false
+                },
+                minutes: {
                     type: DataTypes.INTEGER,
                     allowNull: false
                 },
@@ -18,26 +20,23 @@ module.exports = class DailyInvestment extends Sequelize.Model {
                     type: DataTypes.DATE,
                     allowNull: false
                 },
-                user_id:{
+                user_id: {
                     type: DataTypes.INTEGER,
                     allowNull: false
                 }
             },
             { 
-            
-                    sequelize, // Sequelize 인스턴스 전달
-                    modelName: 'DailyInvestment', // 모델 이름
-                    tableName: 'daily_investments', // 테이블 이름
-                    timestamps: true, // timestamps 설정 (생성일 및 수정일)
-                    underscored: true, // 카멜케이스 대신 스네이크 케이스 사용
-                    paranoid: true // soft delete 설정
-            
+                sequelize, // Sequelize 인스턴스 전달
+                modelName: 'DailyInvestment', // 모델 이름
+                tableName: 'daily_investments', // 테이블 이름
+                timestamps: true, // timestamps 설정 (생성일 및 수정일)
+                underscored: true, // 카멜케이스 대신 스네이크 케이스 사용
+                paranoid: true // soft delete 설정
             }
         );
     }   
 
     static associate(db) {
-
-        db.DailyInvestment.belongsTo(db.User);}
-
+        db.DailyInvestment.belongsTo(db.User);
+    }
 }
